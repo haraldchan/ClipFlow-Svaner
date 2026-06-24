e5d5ac974fdcb8e13b809dee028ba1de8ce8dc1f
haraldchan <katahane42@gmail.com>
2026-06-24 13:07:13 +0800

    feat: added dicts

---
+++ ws-reader/dicts/misc-dict.js
@@ -0,0 +1,22 @@
+function getGuestType (typeCode, nationalityArea = '') {
+	switch (typeCode) {
+		case '100':
+			return '内地旅客';
+			break;
+		case '200':
+			return nationalityArea === 'CHN' ? '内地旅客' : '港澳台旅客';
+			break;
+        case '300':
+            return '国外旅客'
+		default:
+			return '';
+			break;
+	}
+};
+
+const cardTypes = new Map([
+	['11', '身份证'],
+	['14', '普通护照'],
+	['93', '国内护照'],
+	['60', '港澳居民来往内地通行证'],
+]);
