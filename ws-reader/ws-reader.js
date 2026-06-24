f31e5a6ffd203f76a0965713a8aa7860e7cfe2d0
haraldchan <katahane42@gmail.com>
2026-06-24 13:07:45 +0800

    refactor: updated form updating and send event

--- ws-reader/ws-reader.js
+++ ws-reader/ws-reader.js
@@ -78,60 +78,150 @@ async function connectOrDisconnect() {
 }
 
 // form elements
+const form = document.querySelector('.passport-card');
 const passportPhotoImg = document.querySelector('.passport-photo');
+
 const fullName = document.getElementById('fullname');
+fullName.addEventListener(
+	'change',
+	(e) => (currentData.data.name = e.target.value),
+);
+
 const lastName = document.getElementById('lastname');
+lastName.addEventListener(
+	'change',
+	(e) => (currentData.lastName = e.target.value),
+);
+
 const firstName = document.getElementById('firstname');
+firstName.addEventListener(
+	'change',
+	(e) => (currentData.firstName = e.target.value),
+);
+
 const roomNum = document.getElementById('room-num');
 const tel = document.getElementById('tel');
-const idNum = document.getElementById('id-num');
-const idType = document.getElementById('id-type');
-const gender = document.getElementById('gender');
-const birthday = document.getElementById('birthday');
+
 const guestType = document.getElementById('guest-type');
+guestType.addEventListener(
+	'change',
+	(e) => (currentData.guestType = e.target.value),
+);
+
+const regionList = document.getElementById('region-list');
 const region = document.getElementById('region');
+region.addEventListener('change', (e) => {
+	if (currentData.guestType === '内地旅客') {
+		currentData.nationalityArea = 'CHN';
+		return;
+	}
+	currentData.nationalityArea = e.target.value;
+});
+
+const address = document.getElementById('address');
+address.addEventListener(
+	'change',
+	(e) => (currentData.address = e.target.value),
+);
+
+const idNum = document.getElementById('id-num');
+idNum.addEventListener('change', (e) => (currentData.cardNo = e.target.value));
+
+const idType = document.getElementById('id-type');
+idType.addEventListener(
+	'change',
+	(e) => (currentData.cardType = e.target.value),
+);
+
+const gender = document.getElementById('gender');
+gender.addEventListener('change', (e) => (currentData.sex = e.target.value));
+
+const birthday = document.getElementById('birthday');
+birthday.addEventListener(
+	'change',
+	(e) => (currentData.birthday = e.target.value),
+);
+
 const validDate = document.getElementById('valid-date');
+validDate.addEventListener(
+	'change',
+	(e) => (currentData.validDate = e.target.value),
+);
 
 // ui functions
-function handleMessageDisplay(data) {
-	currentData = data;
 
-	passportPhotoImg.src = `data:image/jpeg;base64,${data.data.curPhoto}`
-	fullName.value = data.data.name ?? "";
-	lastName.value = data.data.lastName ?? "";
-	firstName.value = data.data.firstName ?? "";
-	idNum.value = data.data.cardNo;
-	gender.selectedIndex = data.data.sex - 1;
-	birthday.value = data.data.birthday
-	validDate.value = data.data.validDate;
+function createOption(select, textContent, value = null) {
+	const option = document.createElement('option');
+
+	if (value !== null) option.value = value;
+	option.textContent = textContent;
+
+	select.appendChild(option);
+}
+
+function handleMessageDisplay(data) {
+	currentData = data.data;
+
+	// updated inputs & non-dynamic selects
+	passportPhotoImg.src = `data:image/jpeg;base64,${currentData.curPhoto}`;
+	fullName.value = currentData.name ?? '';
+	lastName.value = currentData.lastName ?? '';
+	firstName.value = currentData.firstName ?? '';
+	address.value = currentData.address;
+	idNum.value = currentData.cardNo;
+	birthday.value = currentData.birthday;
+	validDate.value = currentData.validDate;
+
+	// update selects
+	gender.selectedIndex = currentData.sex;
+	idType.value = currentData.cardType;
+	region.value = nationalityAreas[currentData.nationalityArea];
+
+	const curGuestType = getGuestType(
+		currentData.guestType,
+		currentData.hasOwnProperty('nationalityArea')
+			? currentData.nationalityArea
+			: '',
+	);
+	switch (curGuestType) {
+		case '内地旅客':
+			guestType.selectedIndex = 1;
+			break;
+		case '港澳台旅客':
+			guestType.selectedIndex = 2;
+			break;
+		case '国外旅客':
+			guestType.selectedIndex = 3;
+			break;
+	}
 }
 // handleMessageDisplay({
-//     code: 0, // if success
-//     command: "scan",      // can only use "scan"
-//     data: {
-//         address:   "",
-//         adminDivision: "",
-//         birthday:  "1990-03-30", // yyyy-MM-dd,
-//         cardNo:    "235434534",
-//         cardType:  "14", // 14=foreign passport
-//         curPhoto:  "awf", // base64 photo for passfoto
-//         firstName: "mookie",
-//         lastName:  "bets",
-//         nationalityArea: "MYS", // iso-3166-1 alpha3
-//         photo:     "",
-//         guestType: "300", // numbered key
-//         name:      "bets mookie", // full hanzi name
-//         nation:    "",
-//         sex:       "2", // 1=male, 2=female
-//         signDate:  "--",
-//         signOrg:   "",
-//         signPlace: "string",
-//         validDate: "1999-12-12"
-//     },
-//     message: ""
-// })
+// 	code: 0, // if success
+// 	command: 'scan', // can only use "scan"
+// 	data: {
+// 		address: '',
+// 		adminDivision: '',
+// 		birthday: '1990-03-30', // yyyy-MM-dd,
+// 		cardNo: '235434534',
+// 		cardType: '11', // 14=foreign passport
+// 		curPhoto: 'awf', // base64 photo for passfoto
+// 		firstName: 'mookie',
+// 		lastName: 'bets',
+// 		nationalityArea: 'HKG', // iso-3166-1 alpha3
+// 		photo: '',
+// 		guestType: '100', // numbered key
+// 		name: '姆基贝茨', // full hanzi name
+// 		nation: '',
+// 		sex: '2', // 1=male, 2=female
+// 		signDate: '--',
+// 		signOrg: '',
+// 		signPlace: 'string',
+// 		validDate: '1999-12-12',
+// 	},
+// 	message: '',
+// });
 
-
+// btns & events
 function sendCommand(e) {
 	const option = e.target.id.replace('-btn', '');
 
@@ -150,7 +240,6 @@ const connBtn = document.querySelector('.connection-btn');
 connBtn.addEventListener('click', async () => {
 	connectOrDisconnect();
 });
-// connBtn.click();
 
 const readBtn = document.getElementById('read-btn');
 readBtn.addEventListener('click', sendCommand);
@@ -158,7 +247,30 @@ readBtn.addEventListener('click', sendCommand);
 const scanBtn = document.getElementById('scan-btn');
 scanBtn.addEventListener('click', sendCommand);
 
+const sendBtn = document.getElementById('send-btn');
+sendBtn.addEventListener('click', async () => {
+	if (!form.reportValidity()) return;
+
+	const sendClip = {
+		identifier: '3ed542123e774d45203ff60175cb614e',
+		tsId: Date.now(),
+		roomNum: roomNum.value,
+		tel: tel.value,
+		guestType: guestType.value,
+		idType: idType.value,
+		region: region.value,
+		data: currentData,
+	};
+
+	await navigator.clipboard.writeText(JSON.stringify(sendClip));
+});
+
+// init
 function onLaunch() {
-	connectOrDisconnect()
+	for (const code in nationalityAreas) {
+		createOption(regionList, code, nationalityAreas[code]);
+	}
+
+	connectOrDisconnect();
 }
-onLaunch()
+onLaunch();
