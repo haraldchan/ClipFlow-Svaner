161dec4c36a0af89bbe4ff891c7b72502a615797
haraldchan <katahane42@gmail.com>
2026-06-26 12:37:18 +0800

    rerfactor: added card type: `港澳居民来往内地通行证（非中国籍）`

--- ws-reader/dicts/misc-dict.js
+++ ws-reader/dicts/misc-dict.js
@@ -1,4 +1,4 @@
-function getGuestType (typeCode, nationalityArea = '') {
+function getGuestType(typeCode, nationalityArea = '') {
 	switch (typeCode) {
 		case '100':
 			return '内地旅客';
@@ -6,13 +6,13 @@ function getGuestType (typeCode, nationalityArea = '') {
 		case '200':
 			return nationalityArea === 'CHN' ? '内地旅客' : '港澳台旅客';
 			break;
-        case '300':
-            return '国外旅客'
+		case '300':
+			return '国外旅客';
 		default:
 			return '';
 			break;
 	}
-};
+}
 
 const cardTypes = new Map([
 	['11', '身份证'],
@@ -20,4 +20,5 @@ const cardTypes = new Map([
 	['93', '国内护照'],
 	['60', '港澳居民来往内地通行证'],
 	['16', '台湾居民来往大陆通行证'],
+	['56', '港澳居民来往内地通行证（非中国籍）'],
 ]);
