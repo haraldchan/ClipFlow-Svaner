a76be7729268ebe7ec94191753562143b34c6f27
haraldchan <116058185+haraldchan@users.noreply.github.com>
2026-07-04 16:42:07 +0800

    fix: fixed idType assignment to use groupedCardTypes

--- ws-reader/js/action-bar.js
+++ ws-reader/js/action-bar.js
@@ -119,7 +119,7 @@ class ActionBar extends HTMLElement {
                 roomNum: FormContent.roomNum.value,
                 tel: FormContent.tel.value,
                 guestType: FormContent.guestType.value,
-                idType: cardTypes.get(FormContent.idType.value),
+                idType: groupedCardTypes.get(FormContent.guestType.value).get(FormContent.idType.value),
                 region: FormContent.region.value,
                 data: currentData,
             };
