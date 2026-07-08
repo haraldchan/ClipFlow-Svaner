1cb572a7c3d674473c35a31303c6f592d7a0d66a
haraldchan <katahane42@gmail.com>
2026-07-09 00:45:54 +0800

    feat: new html helper function

---
+++ ws-reader/lib/html-helpers.js
@@ -0,0 +1,29 @@
+function html(strings, ...values) {
+    let result = ''
+
+    for (let i = 0; i < values.length; i++) {
+        result += strings[i]
+        let value = values[i]
+
+        if (
+            value &&
+            Symbol.iterator in Object(value) &&
+            typeof value !== 'string'
+        ) {
+            result += [...value].join('')
+        } else {
+            result += value ?? ''
+        }
+    }
+
+    return result + strings.at(-1)
+}
+
+function each(iterable, render) {
+    let html = ''
+
+    for (const item of iterable)
+        html += render(item)
+
+    return html
+}
