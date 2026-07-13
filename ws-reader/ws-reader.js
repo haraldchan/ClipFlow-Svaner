00c43bbf8b7a5bbdb7ada4eafc5d537d282ecc2f
haraldchan <katahane42@gmail.com>
2026-07-13 11:43:13 +0800

    refactor: updated globals

--- ws-reader/ws-reader.js
+++ ws-reader/ws-reader.js
@@ -1,4 +1,21 @@
 // globals
-let socket = null;
-let currentData = {};
-const identifier = '3ed542123e774d45203ff60175cb614e'; //MD5 hash: ProfileModifyNextLocal
+/** @type {WebSocket | null} */
+let socket = null
+
+const identifier = '3ed542123e774d45203ff60175cb614e' //MD5 hash: ProfileModifyNextLocal
+
+let currentData = {
+    address: "",
+    birthday: "",
+    cardNo: "",
+    cardType: "",
+    curPhoto: "",
+    guestType: "",
+    name: "",
+    nation: "",
+    sex: "",
+    beginDate: "",
+    signDate: "",
+    signOrg: "",
+    validDate: ""
+}
