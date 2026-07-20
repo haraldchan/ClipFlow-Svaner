7219fd56eb9bfa47f19572b0071f8f8ec2b23a1e
haraldchan <katahane42@gmail.com>
2026-07-21 02:09:09 +0800

    refactor: set default value of `currentData` to undefined

--- ws-reader/ws-reader.js
+++ ws-reader/ws-reader.js
@@ -5,18 +5,18 @@ let socket = null
 const identifier = '3ed542123e774d45203ff60175cb614e' //MD5 hash: ProfileModifyNextLocal
 
 let currentData = {
-    address: "",
-    birthday: "",
-    cardNo: "",
-    cardType: "",
-    curPhoto: "",
-    guestType: "",
-    name: "",
-    nation: "",
-    sex: "",
-    beginDate: "",
-    signDate: "",
-    signOrg: "",
-    validDate: "",
-    checkinDate: ""
+    address: undefined,
+    birthday: undefined,
+    cardNo: undefined,
+    cardType: undefined,
+    curPhoto: undefined,
+    guestType: undefined,
+    name: undefined,
+    nation: undefined,
+    sex: undefined,
+    beginDate: undefined,
+    signDate: undefined,
+    signOrg: undefined,
+    validDate: undefined,
+    regTime: undefined,
 }
